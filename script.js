fd80e7d730cf8c01dbf8fff9a7ade24fc2e46e1a
Shamil <sharippay2@icloud.com>
2025-11-30 10:20:11 +0500

    дополонили фильтр

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 // выводит окошко на экран
 // alert('222');
 
+// Только раскрытие текста
+// document.querySelector('.info__link').addEventListener('click', function () {
+//     this.remove()
+//     document.querySelector('.info__text').classList.add('info__text--active');
+// })
+
+let buttonText;
+let buttonText2 = "Скрыть текст";
+
+// Раскрытие-сворачивание
+// document.querySelector('.info__link').addEventListener('click', function () {
+//     document.querySelector('.info__text').classList.toggle('info__text--active');
+//     if (this.textContent == 'Показать полностью') {
+//         this.textContent = "Скрыть текст";
+//     } else {
+//         this.textContent = "Показать полностью";
+//     }
+// })
+
 document.querySelector('.info__link').addEventListener('click', function () {
-    this.remove();
-    document.querySelector('.info__text').classList.add('info__text--active');
-});
+    document.querySelector('.info__text').classList.toggle('info__text--active');
+    buttonText = this.textContent;
+    this.textContent = buttonText2;
+    buttonText2 =  buttonText;
+})
+
