7178eff40d306919c5d507b18b358efd7bdb6449
Shamil <sharippay2@icloud.com>
2025-12-06 11:53:26 +0500

    циклы

--- script.js
+++ script.js
@@ -24,6 +24,110 @@ document.querySelector('.info__link').addEventListener('click', function () {
     document.querySelector('.info__text').classList.toggle('info__text--active');
     buttonText = this.textContent;
     this.textContent = buttonText2;
-    buttonText2 =  buttonText;
+    buttonText2 = buttonText;
 })
 
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    const buttonPopup = document.querySelector('.catalog__filter');
+    const popupFilter = document.querySelector('.popup-filter');
+    if (buttonPopup) {
+        buttonPopup.addEventListener('click', function () {
+
+
+            popupFilter.classList.add('popup-filter--open');
+            document.body.classList.add('no-scroll');
+        });
+    }
+    const closeBtn = document.querySelector('.filters__close');
+    if (closeBtn) {
+        closeBtn.addEventListener('click', function () {
+
+            popupFilter.classList.remove('popup-filter--open');
+            document.body.classList.remove('no-scroll');
+        });
+    }
+
+    let titles = [
+        'Общее',
+        'Сыр',
+        'Мясо',
+        'Компонент',
+    ];
+
+    let buttons = [
+        [
+            'Хит',
+            'Новинка',
+            'С мясом',
+            'Вегетарианская',
+            'С курицей',
+            'Без лука',
+            'С грибами',
+            'С морепродуктами',
+            'Барбекю',
+        ],
+        [
+            'Реджанито',
+            'Моцарелла',
+            'Чеддер',
+            'С голубой плесенью',
+            'Смесь итальянских сыров',
+            'Мягкий молодой сыр',
+        ],
+        [
+            'Пепперони',
+            'Свинина',
+            'Ветчина',
+            'Бекон',
+            'Говядина',
+            'Чоризо',
+            'Колбаски',
+            'Куриная грудка',
+        ],
+        [
+            'Креветка',
+            'Ананасы',
+            'Шампиньоны',
+            'Лук',
+            'Перец халапеньо',
+            'Орегано',
+            'Зеленый перец',
+            'Томаты',
+            'Чеснок',
+            'Красный перец',
+            'Оливки',
+            'Маслины',
+            'Клубника',
+            'Смесь итальянских трав',
+        ],
+    ];
+
+    const groups = document.querySelector('.filters__body')
+    if (groups) {
+        let groupHtml = '';
+        for (let i = 0; i < titles.length; i++) {
+            let buttonsHtml = '';
+            for (let j = 0; j < titles.length; j++) {
+                buttonsHtml = buttonsHtml + `<button class="filter-tag">${buttons[i][j]}</button>`
+            }
+            groupHtml = groupHtml + `<div class="filters__group">
+                    <h3 class="filters__group-title">${titles[i]}</h3>
+                    <div class="filters__list filter-common">
+                       ${buttonsHtml}
+                        
+                </div>`
+        }
+        groups.innerHTML=groupHtml;
+    }
+
+});
+
+// for (let i = 0; i < buttons.length; i++) {
+//            const element = buttons[i];
+//           let newElement = document.createElement('button')
+//           newElement.classList.add('filter-tag');
+//          newElement.textContent = buttons[i]
+//         list.append(newElement)
+//      }
